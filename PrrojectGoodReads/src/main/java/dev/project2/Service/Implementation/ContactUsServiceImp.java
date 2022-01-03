c0755d8a1713a4a40b914e298a87ee56089758e4
amandue10 <amandue10.ag@gmail.com>
2022-01-03 16:45:57 -0700

    working on service layer, saved for break

--- PrrojectGoodReads/src/main/java/dev/project2/Service/Implementation/ContactUsServiceImp.java
+++ PrrojectGoodReads/src/main/java/dev/project2/Service/Implementation/ContactUsServiceImp.java
@@ -1,43 +1,56 @@
 package dev.project2.Service.Implementation;
 
+import dev.project2.DAO.Abstract.ContactUsAbstract;
 import dev.project2.Entities.ContactUs;
+import dev.project2.Exception.ContactUsNotFound;
 import dev.project2.Service.Abstract.ContactUsAbstractService;
 
 import java.util.List;
 
 public class ContactUsServiceImp implements ContactUsAbstractService {
+ContactUsAbstract contactDao;
+
+public ContactUsServiceImp(ContactUsAbstract contactDao){
+    this.contactDao = contactDao;
+}
+
     @Override
     public ContactUs createContact(ContactUs contactUs) {
-        return null;
+        return this.contactDao.createContact(contactUs);
     }
 
     @Override
     public ContactUs getContactUsById(int contactId) {
-        return null;
+        try{
+            ContactUs contactUs = this.contactDao.getContactUsById(contactId);
+            return contactUs;
+        } catch (ContactUsNotFound e){
+            throw new ContactUsNotFound("Contact Request Not Found");
+        }
     }
 
     @Override
     public List<ContactUs> getAllContactRequests() {
-        return null;
+        return this.contactDao.getAllContactRequests();
     }
 
     @Override
     public List<ContactUs> getPendingContactRequests() {
-        return null;
+        return this.contactDao.getPendingContactRequests();
     }
 
     @Override
     public List<ContactUs> getCompletedContactRequests() {
-        return null;
+        return this.contactDao.getCompletedContactRequests();
     }
 
     @Override
     public boolean updateContactRequestById(int contactId) {
-        return false;
+        return this.contactDao.updateContactRequestById(contactId);
     }
 
     @Override
     public boolean deleteContactRequestById(int contactId) {
-        return false;
+        return this.contactDao.deleteContactRequestById(contactId);
     }
 }
