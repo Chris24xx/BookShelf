16fa38f43fda0b6172763aad0580dd8632aebbdb
KKarnoski <82429567+bluedragonscales@users.noreply.github.com>
2022-01-04 15:07:40 -0800

    Now tentatively finished the service layer.

--- PrrojectGoodReads/src/test/java/dev/project2/Service/Implementation/MediaServiceTests.java
+++ PrrojectGoodReads/src/test/java/dev/project2/Service/Implementation/MediaServiceTests.java
@@ -1,7 +1,6 @@
 package dev.project2.Service.Implementation;
 import dev.project2.DAO.Abstract.MediaDAO;
 import dev.project2.DAO.Implementation.MediaDAOImp;
-import dev.project2.Entities.Media;
 import dev.project2.Exception.ItemNotFound;
 import dev.project2.Exception.TitleNotFound;
 import dev.project2.Service.Abstract.MediaService;
@@ -12,15 +11,11 @@ public class MediaServiceTests {
     MediaDAO mediaDAO = new MediaDAOImp();
     MediaService mediaService = new MediaServiceImp(mediaDAO);
 
-//    @Test(expectedExceptions = PlayerNotFound.class, expectedExceptionsMessageRegExp = "Player not found")
-//    void badIdForGettingPlayer(){
-//        Player player = playerServices.getPlayerByIdService(10000);
-//    }
 
 
     @Test(expectedExceptions = TitleNotFound.class, expectedExceptionsMessageRegExp = "Title was not found")
     void testGetMediaByTitleService(){
-        Media media = mediaService.getMediaByTitleService("Capture the Flag");
+        mediaService.getMediaByTitleService("Capture the Flag");
     }
 
 
