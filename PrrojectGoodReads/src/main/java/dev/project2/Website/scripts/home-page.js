d44b73ac7e19824d580277ce842ed74d34e8fe81
KKarnoski <82429567+bluedragonscales@users.noreply.github.com>
2022-01-03 17:20:10 -0800

    Made a bunch of website pages.

---
+++ PrrojectGoodReads/src/main/java/dev/project2/Website/scripts/home-page.js
@@ -0,0 +1,12 @@
+function openTab(evt, tabName) {
+    let tabcontent = document.getElementsByClassName("tabcontent");
+    for (let i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    };
+    let tablink = document.getElementsByClassName("tablink");
+    for (let i = 0; i < tablink.length; i++) {
+      tablink[i].className = tablink[i].className.replace(" active", "");
+    };
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+  };
