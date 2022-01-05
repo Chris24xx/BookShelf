b7cd7e95cab1860cbd16eb6719f0f4be720efa57
amandue10 <amandue10.ag@gmail.com>
2022-01-05 15:33:10 -0700

    save working code on controller for contact us

--- PrrojectGoodReads/src/main/java/dev/project2/controllers/ContactUsController.java
+++ PrrojectGoodReads/src/main/java/dev/project2/controllers/ContactUsController.java
@@ -1,5 +1,38 @@
 package dev.project2.controllers;
 
+import com.google.gson.Gson;
+import dev.project2.Entities.ContactUs;
+import dev.project2.Service.Implementation.ContactUsServiceImp;
+import io.javalin.http.Handler;
+
 public class ContactUsController {
+    ContactUsServiceImp contactService;
+
+    public ContactUsController(ContactUsServiceImp contactService) {
+        this.contactService = contactService;
+    }
+
+    public Handler createContactRequest = ctx -> {
+        Gson gson = new Gson();
+        ContactUs newContactUs = gson.fromJson(ctx.body(), )
+
+    };
+
+    public Handler getPlayerById = ctx -> {
+
+    };
+
+    public Handler getAllContactRequests = ctx -> {
+
+    };
+
+    public Handler getPendingContactRequests = ctx -> {
+
+    };
+
+    public Handler getCompletedContactRequests = ctx -> {
+
+    };
+
 
 }
