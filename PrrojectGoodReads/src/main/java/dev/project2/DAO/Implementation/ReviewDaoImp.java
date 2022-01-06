416d3a73274470538c12688f0671caeea0743b9d
chris <christianayala4@gmail.com>
2022-01-06 13:34:28 -0500

    Finalized Gson changes

--- PrrojectGoodReads/src/main/java/dev/project2/DAO/Implementation/ReviewDaoImp.java
+++ PrrojectGoodReads/src/main/java/dev/project2/DAO/Implementation/ReviewDaoImp.java
@@ -7,6 +7,7 @@ import dev.project2.dbcon.DBConn;
 import java.sql.SQLException;
 import java.sql.*;
 import java.time.Instant;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,7 +21,7 @@ public class ReviewDaoImp implements ReviewAbstract  {
             String sql;
             sql = "insert into project2.review values(default,null,?,?,?,?,?)";
             PreparedStatement state = conn.prepareStatement(sql,Statement.RETURN_GENERATED_KEYS);
-            state.setString(1, review.getCreatedAt());
+            state.setString(1, review.getDateAndTime());
             state.setInt(2,review.getUserId());
             state.setInt(3,review.getRating());
             state.setString(4,review.getUserReview());
@@ -127,18 +128,26 @@ public class ReviewDaoImp implements ReviewAbstract  {
 
     @Override
     public List<Review> getPendingReviews() {
-        try(Connection connection = DBConn.createConnection()) {
+        try(Connection conn = DBConn.createConnection()) {
             String sql;
-            sql = "select status, user_review from project2.review where status is null";
-            Statement statement = connection.createStatement();
+            sql = "select * from project2.review where status is null";
+            Statement statement = conn.createStatement();
             ResultSet resultSet = statement.executeQuery(sql);
             List<Review> reviewList = new ArrayList<>();
-            while (resultSet.next()){
+            while(resultSet.next()){
                 Review review = new Review(
+                        resultSet.getInt("review_id"),
                         resultSet.getObject("status", Boolean.class),
+                        resultSet.getString("created_at"),
+                        resultSet.getInt("media_id"),
+                        resultSet.getInt("user_id"),
+                        resultSet.getInt("rating"),
                         resultSet.getString("user_review")
+
                 );
                 reviewList.add(review);
+
+
             }
             return reviewList;
 
@@ -150,27 +159,34 @@ public class ReviewDaoImp implements ReviewAbstract  {
 
     @Override
     public List<Review> notNullReviews() {
-        try(Connection connection = DBConn.createConnection()) {
-            String sql;
-            sql = "select status, user_review from project2.review where status is not null";
-            Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery(sql);
-            List<Review> reviewList= new ArrayList<>();
-            while (resultSet.next()){
-                Review review = new Review(
-                        resultSet.getObject("status", Boolean.class),
-                        resultSet.getString("user_review")
-                );
+             try(Connection conn = DBConn.createConnection()) {
+                String sql;
+                sql = "select * from project2.review where status is not null";
+                Statement statement = conn.createStatement();
+                ResultSet resultSet = statement.executeQuery(sql);
+                List<Review> reviewList = new ArrayList<>();
+                while(resultSet.next()){
+                    Review review = new Review(
+                            resultSet.getInt("review_id"),
+                            resultSet.getObject("status", Boolean.class),
+                            resultSet.getString("created_at"),
+                            resultSet.getInt("media_id"),
+                            resultSet.getInt("user_id"),
+                            resultSet.getInt("rating"),
+                            resultSet.getString("user_review")
 
-                reviewList.add(review);
+                    );
+                    reviewList.add(review);
+
+
+                }
+                return reviewList;
+
+            } catch (SQLException e) {
+                e.printStackTrace();
+                return null;
             }
-            return reviewList;
-
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-            return null;
-        }
     }
-
 }
+
+
