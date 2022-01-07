bc1261b82ac6f689f097b19ccdc0b48f815fd141
chris <christianayala4@gmail.com>
2022-01-07 12:44:32 -0500

    finished moderator review on js

--- PrrojectGoodReads/src/main/java/dev/project2/Website/scripts/moderator.js
+++ PrrojectGoodReads/src/main/java/dev/project2/Website/scripts/moderator.js
@@ -1,67 +1,67 @@
 // WORKING TABS
 function openTab(evt, tabName) {
-    let tabcontent = document.getElementsByClassName("tabcontent");
-    for (let i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
-    };
-    let tablink = document.getElementsByClassName("tablink");
-    for (let i = 0; i < tablink.length; i++) {
-      tablink[i].className = tablink[i].className.replace(" active", "");
-    };
-    document.getElementById(tabName).style.display = "block";
-    evt.currentTarget.className += " active";
+  let tabcontent = document.getElementsByClassName("tabcontent");
+  for (let i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
   };
+  let tablink = document.getElementsByClassName("tablink");
+  for (let i = 0; i < tablink.length; i++) {
+    tablink[i].className = tablink[i].className.replace(" active", "");
+  };
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.className += " active";
+};
 
 
-  //Contact us List of Completed and Pending Function Constants
-  const pendingTable = document.getElementById("pending-messages-table");
-  const pendingTableBody = document.getElementById("pending-message-body");
-  const completedTable = document.getElementById("completed-messages-table");
-  const completedTableBody = document.getElementById("completed-message-body");
+//Contact us List of Completed and Pending Function Constants
+const pendingTable = document.getElementById("pending-messages-table");
+const pendingTableBody = document.getElementById("pending-message-body");
+const completedTable = document.getElementById("completed-messages-table");
+const completedTableBody = document.getElementById("completed-message-body");
 // Get Pending Contact Requests
-  async function getPendingContactRequests(){
-    let url = "http://localhost:8080/contactRequests/pending";
+async function getPendingContactRequests() {
+  let url = "http://localhost:8080/contactRequests/pending";
 
-    let response = await fetch(url);
-    
-    if (response.status === 200){
-        let body = await response.json();
-        populateData(body);
-    } else {
-        alert("There was a problem trying to get pending contact requests: sorry!");
-    }
+  let response = await fetch(url);
+
+  if (response.status === 200) {
+    let body = await response.json();
+    populateData(body);
+  } else {
+    alert("There was a problem trying to get pending contact requests: sorry!");
+  }
 }
 
 // Get Completed Contact Requests -----------------------------------------
 
-async function getCompletedContactRequests(){
-    let url = "http://localhost:8080/contactRequests/completed";
+async function getCompletedContactRequests() {
+  let url = "http://localhost:8080/contactRequests/completed";
 
-    let response = await fetch(url);
-    
-    if (response.status === 200){
-        let body = await response.json();
-        populateDataComp(body);
-    } else {
-        alert("There was a problem trying to get completed contact requests: sorry!");
-    }
+  let response = await fetch(url);
+
+  if (response.status === 200) {
+    let body = await response.json();
+    populateDataComp(body);
+  } else {
+    alert("There was a problem trying to get completed contact requests: sorry!");
+  }
 }
 
 // Populate data for Pending contact us table
-function populateData(responseBody){
-  for (let contact_us of responseBody){
-      let tableRow = document.createElement("tr");
-      tableRow.innerHTML = `<td>${contact_us.contactId}</td><td>${contact_us.email}</td><td>${contact_us.fullName}</td><td>${contact_us.issueMessage}</td><td>${contact_us.status}</td>`;
-      pendingTableBody.appendChild(tableRow);
+function populateData(responseBody) {
+  for (let contact_us of responseBody) {
+    let tableRow = document.createElement("tr");
+    tableRow.innerHTML = `<td>${contact_us.contactId}</td><td>${contact_us.email}</td><td>${contact_us.fullName}</td><td>${contact_us.issueMessage}</td><td>${contact_us.status}</td>`;
+    pendingTableBody.appendChild(tableRow);
   }
 }
 
 // Populate data for Completed contact us table
-function populateDataComp(responseBody){
-  for (let contact_us of responseBody){
-      let tableRow = document.createElement("tr");
-      tableRow.innerHTML = `<td>${contact_us.contactId}</td><td>${contact_us.email}</td><td>${contact_us.fullName}</td><td>${contact_us.issueMessage}</td><td>${contact_us.status}</td>`;
-      completedTableBody.appendChild(tableRow);
+function populateDataComp(responseBody) {
+  for (let contact_us of responseBody) {
+    let tableRow = document.createElement("tr");
+    tableRow.innerHTML = `<td>${contact_us.contactId}</td><td>${contact_us.email}</td><td>${contact_us.fullName}</td><td>${contact_us.issueMessage}</td><td>${contact_us.status}</td>`;
+    completedTableBody.appendChild(tableRow);
   }
 }
 
@@ -72,38 +72,96 @@ getPendingContactRequests()
 //------------------Reviews
 let reviewButton = document.getElementById("pending-reviews")
 reviewButton.addEventListener("click", pendingReviewList)
+reviewButton.addEventListener("click", pastList)
 let pendingReviewTable = document.getElementById("pending-review-table")
-
+let approveReviewTable = document.getElementById("approved-review-table")
+let reviewField = document.getElementById("updateReview")
+let approveButton = document.getElementById("Approve")
+let denyButton = document.getElementById("Deny")
 
 async function pendingReviewList(params) {
   let url = "http://localhost:8080/review/pending";
   let response = await fetch(url);
   if (response.status === 200) {
     let body = await response.json();
-    console.log(body)
+
     populatePendingList(body)
-    
-  }else if (response.status === 404) {
+
+  } else if (response.status === 404) {
     alert("List can not be generated")
-    
+
   } else {
     alert("something went wrong.")
   }
 }
 
-function populatePendingList(responseBody){
+function populatePendingList(responseBody) {
   let array = responseBody
-  for(let reviews of array){
+  for (let reviews of array) {
     let tableRow = document.createElement("tr")
-    tableRow.innerHTML =`<td>${reviews.reviewId}</td><td>${reviews.userReview}</td><td>${reviews.mediaId}</td>`
+    tableRow.innerHTML = `<td>${reviews.reviewId}</td><td>${reviews.userReview}</td><td>${reviews.mediaId}</td>`
     pendingReviewTable.appendChild(tableRow);
   }
-    
-  
+
+
 }
+
+async function pastList() {
+  let url = "http://localhost:8080/review/past";
+  let response = await fetch(url);
+  if (response.status === 201) {
+    let body = await response.json();
+    console.log(body)
+    populatePastList(body)
+
+  } else if (response.status === 404) {
+    alert("List can not be generated")
+
+  } else {
+    alert("something went wrong.")
+  }
+}
+
+function populatePastList(responseBody) {
+  for (let reviews of responseBody) {
+    let table = document.createElement("tr")
+    if (reviews.status === true) {
+      table.innerHTML = `<td>${reviews.reviewId}</td><td>${reviews.userReview}</td><td>${reviews.mediaId}</td>`
+      approveReviewTable.appendChild(table);
+    }
+
+  }
+}
+
+async function updateReview(approval) {
+  let status = approval
+  let url = `http://localhost:8080/review/update/${reviewField.value}`;
+  let response = await fetch(url, { method: "PATCH", headers: { "Content-Type": "application/json" }, body: JSON.stringify({ ["status"]: status }) });
+      if (response.status === 201) {
+        alert("success");
+        if(status === false){
+          let url = `http://localhost:8080/review/delete/${reviewField.value}`;
+          let response = await fetch(url, { method: "DELETE", headers: { "Content-Type": "application/json" }});
+          if(response.status === 200){
+            alert("success")
+          }
+          
+        }
+    
+      } else {
+        alert("something went wrong.");
+      }
+ }
+  
+  
+
+
+  
+
+
 
 
 // LOGOUT
-function logout(){
-    window.location.href = "login.html";
-  };
+function logout() {
+  window.location.href = "login.html";
+};
