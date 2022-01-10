41cdf870ee8e9b7f020edcaa3de47cc91567f2b4
chris <christianayala4@gmail.com>
2022-01-10 16:00:40 -0500

    Steps started

---
+++ PrrojectGoodReads/src/test/java/dev/project2/steps/managerReviewSteps.java
@@ -0,0 +1,46 @@
+package dev.project2.steps;
+import io.cucumber.java.en.*;
+
+public class managerReviewSteps {
+    @Given("Moderator is on the moderator home page")
+    public void moderator_is_on_the_moderator_home_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("the Moderator clicks the pending review tab")
+    public void the_moderator_clicks_the_pending_review_tab() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("a pending list is returned to the moderator")
+    public void a_pending_list_is_returned_to_the_moderator() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("an approved list is returned to the moderator")
+    public void an_approved_list_is_returned_to_the_moderator() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+
+    @When("the moderator inputs a mediaId into a field")
+    public void theModeratorInputsAMediaIdIntoAField() {
+        
+    }
+
+    @When("the moderator clicks the approve button")
+    public void theModeratorClicksTheApproveButton() {
+        
+    }
+
+    @Then("an alert will appear")
+    public void anAlertWillAppear() {
+        
+    }
+
+    @When("the moderator clicks the deny button")
+    public void theModeratorClicksTheDenyButton() {
+    }
+}
--- PrrojectGoodReads/src/test/java/dev/resource/Features/managerReview.feature
+++ PrrojectGoodReads/src/test/java/dev/resource/Features/managerReview.feature
@@ -2,20 +2,20 @@ Feature:Review Management
   Scenario: as a moderator I should be able to get a list of pending reviews
     Given Moderator is on the moderator home page
     When the Moderator clicks the pending review tab
-    Then a list is returned to the moderator
+    Then a pending list is returned to the moderator
 
 
   Scenario: as a moderator I should be able to get a list of approved reviews
     Given Moderator is on the moderator home page
     When the Moderator clicks the pending review tab
-    Then a list is returned to the moderator
+    Then an approved list is returned to the moderator
 
   Scenario: as a moderator I should be able to approve a review
     Given Moderator is on the moderator home page
     When the Moderator clicks the pending review tab
-    When the moderator inputs a number value
+    When the moderator inputs a mediaId into a field
     When the moderator clicks the approve button
-    Then an a alert appears indicating success
+    Then an alert will appear
 
   Scenario: as a moderator I should be able to deny a review
     Given Moderator is on the moderator home page
