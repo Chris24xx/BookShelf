d21a8f6a6707f8d38b354dcfac339ce301f330e1
chris <christianayala4@gmail.com>
2022-01-12 18:21:21 -0500

    e2e finished

--- PrrojectGoodReads/src/test/java/dev/project2/DAO/Implementation/ReviewDaoImpTest.java
+++ PrrojectGoodReads/src/test/java/dev/project2/DAO/Implementation/ReviewDaoImpTest.java
@@ -16,7 +16,7 @@ public class ReviewDaoImpTest {
 
     @Test
     public void testCreateReview() {
-        Review review = new Review(1,null,"now",25,1,5,"test");
+        Review review = new Review(1,null,"now",30,1,5,"test");
         Review returnedReview = dao.createReview(review);
         Assert.assertTrue(returnedReview.getReviewId() != 0);
     }
